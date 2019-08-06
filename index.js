a9027d6b45da0645210fece679df6360e753306a
AsheN <ashengale01@gmail.com>
2019-08-06 18:53:00 +0200

    test

--- index.js
+++ index.js
@@ -447,7 +447,7 @@ const fnct = {
         }
     }, 
     'approveChar': function(message, reaction, user) {
-        util.log(reaction.name, `debug`, util.logLevel.INFO);
+        util.log("`"+reaction.name+"`", `debug`, util.logLevel.INFO);
         if (_.isEqual(message.channel.name, "📃character-submission") && _.isEqual(reaction.name, ":white_check_mark:") && _.isEqual(user, AsheN)) {
             util.log(message.channel + reaction.name + user, `debug`, util.logLevel.INFO);
         } 
